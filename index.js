e5c2888e46b401305c0a86a5a80b7f8ef063eab8
NavyaPaulose <navyapaulose04@gmail.com>
2025-10-04 20:42:12 +0530

    e-back

---
+++ index.js
@@ -0,0 +1,33 @@
+// import json server
+
+const jsonServer = require('json-server')
+
+// 5.import cors
+
+const cors = require('cors')
+
+// 2.create a server using json-server
+
+const server = jsonServer.create()
+
+
+// 3. setup a middleware
+const middleware=jsonServer.defaults()
+
+// 4.setup routes
+const route= jsonServer.router('db.json')
+
+// 6.use  implementation
+
+server.use(cors())
+server.use(middleware)
+server.use(route)
+
+// 7.create a port
+const PORT= process.env.PORT||3000
+
+// Start the server
+
+server.listen(PORT,()=>{
+    console.log("server is on port:",PORT);
+})
